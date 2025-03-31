bd3dd62c73ff3baa1ccf9dd3954174ec5c2a93e2
Felipe Mengoni <felipemengoninicolas@gmail.com>
2025-03-31 12:46:22 -0300

    modulos importados y programa funcionando

--- prueba.py
+++ prueba.py
@@ -1,3 +1,5 @@
+from src import utils # en vez de importar una funcion en especifico, entro en la carpeta "src" e importo todas las funciones de utils
+
 rounds = [
     {
         'Shadow': {'kills': 2, 'assists': 1, 'deaths': True},
@@ -36,73 +38,19 @@ rounds = [
     }
 ]
 
-def imprimir(rounds):
-    # funcion para imprimir ordenado y con tabla 
-    print('-------------------------------------------------------')
-    print('Jugador    Kills    Assists    Deaths    MVPs    Puntos') 
-    print('-------------------------------------------------------')
-
-    ordenado = dict(sorted(contador.items(), key=lambda x: x[1]['Puntos'], reverse = True))
-
-    for jugador in ordenado:
-
-        space = 11 - int(len(jugador))
-        print(jugador, end='')
-        print(space*' ', end= '')
-        
-        for metrica in ordenado[jugador]:
-            print(ordenado[jugador][metrica], end='')
-            space = int(len(metrica)) + 4 - len(str(ordenado[jugador][metrica])) 
-            print((space)*' ', end= '')
-        
-        print()
-
-def calcular_puntos(jug):
-    # linea que suma los puntos de cada jugador por ronda 
-    return jug['kills'] * 3 + jug['assists'] - jug['deaths'] 
-
-def procesar_ronda(round):
-# inicializamos los puntos y el mvp de cada ronda
-    max_p = 0
-    j_mvp = ''
-    
-    for jugador in round:
-
-        # asignamos el diccionario de cada jugador a una variable para hacerlo mas sencillo
-        jug = round[jugador]
-
-        # calculamos los puntos de la ronda del jugador y los sumamos
-        puntos = calcular_puntos(jug)
-        contador[jugador]['Puntos'] = contador[jugador]['Puntos'] + puntos
-
-        # sumanos kills, assists y deaths
-        contador[jugador]['kills'] = contador[jugador]['kills'] + jug['kills']
-        contador[jugador]['assists'] = contador[jugador]['assists'] + jug['assists']
-        if jug['kills'] == True:
-            contador[jugador]['deaths'] += 1
-            
-        # obtenemos el MVP
-        if puntos > max_p:
-            j_mvp = jugador
-            max_p = puntos
-    
-    # sumamos el MVP
-    contador[j_mvp]['mvps'] += 1
-
 # Crear diccionario contador 
-
 contador = {}
 for i in rounds[0]: #i es el jugador
     contador[i] = {'kills': 0, 'assists': 0, 'deaths': 0, 'mvps': 0, 'Puntos': 0}
 
 for n_round in range(len(rounds)):
-    procesar_ronda(rounds[n_round])
+    utils.procesar_ronda(rounds[n_round], contador)
     print()
     print(f'Ronda nro {n_round + 1}:')
-    imprimir(contador)
+    utils.imprimir(contador)
 
 print()
 print('Partida terminada: resultado final')
-imprimir(contador)
+utils.imprimir(contador)
 
 # El programa esta hecho para que si la cantidad de kills/asistencias/muertes es mas de las que se piden, la impresion se de de forma ordenada
